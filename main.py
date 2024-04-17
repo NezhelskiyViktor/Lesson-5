8de03f552b1f0d4a1b39f66ab6f450631e3ad483
NezhelskiyViktor <nezhelskiy@yandex.ru>
2024-04-17 20:41:11 +0300

    задача Магазины решена

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
 """
-Создай класс `Task`, который позволяет управлять задачами (делами).
-У задачи должны быть атрибуты: описание задачи, срок выполнения и
-статус (выполнено/не выполнено). Реализуй функцию для добавления
-задач, отметки выполненных задач и вывода списка текущих
-(не выполненных) задач.
+Ты разрабатываешь программное обеспечение для сети магазинов.
+Каждый магазин в этой сети имеет свои особенности, но также
+существуют общие характеристики, такие как адрес, название
+и ассортимент товаров. Ваша задача — создать класс Store,
+который можно будет использовать для создания различных магазинов.
 """
 
 
-class Task:
-    def __init__(self, description, due_date):
-        self.description = description
-        self.due_date = due_date
-        self.completed = False
+# Шаблон магазина
+class Store:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.items = {}
 
-    def mark_as_completed(self):
-        self.completed = True
+    # метод для добавления товара в ассортимент магазина
+    def add_item(self, product, price):
+        self.items[product] = price
 
+    # метод для удаления товара из ассортимента.
+    def remove_item(self, product):
+        self.items.pop(product)
 
-tasks = []  # Список задач
+    # метод для получения цены товара по его названию. Если товар отсутствует, возвращайте None
+    def get_price(self, product):
+        return self.items.get(product)
 
-
-# Функция добавления задачи
-def add_task(description, due_date):
-    tasks.append(Task(description, due_date))
-
-
-# Функция отметки выполнения задачи по имени задачи
-def mark_task_completed(done_task):
-    for t in tasks:
-        if t.description == done_task:
-            t.mark_as_completed()
-            break
-
-
-# Функция вывода списка текущих задач
-def print_tasks_completed():
-    print("Текущие задачи:")
-    for t in tasks:
-        if not t.completed:
-            print(f"{t.description} надо выполнить до {t.due_date}")
+    # метод для изменения цены товара по его названию.
+    def set_price(self, product, new_price):
+        if self.items.get(product):
+            self.items[product] = new_price
 
 
-print("Создаем задачи 1 - 4:")
-tasks.append(Task("Задача 1", "20.05.2024"))
-tasks.append(Task("Задача 2", "02.05.2024"))
-tasks.append(Task("Задача 3", "22.05.2024"))
-tasks.append(Task("Задача 4", "12.05.2024"))
-print_tasks_completed()  # список текущих задач
+store1 = Store('"Магнит"', "ул.Красная 1")
+store2 = Store('"Еда"', "ул.Красная 12")
+store3 = Store('"Хозмаг"', "ул.Красная 123")
+print(f"""
+У нас есть три магазина:
+{store1.name} по адресу: {store1.address}
+{store2.name} по адресу: {store2.address}
+{store3.name} по адресу: {store3.address}
+""")
 
-mark_task_completed("Задача 2")  # Задача 2 выполнена
-mark_task_completed("Задача 4")  # Задача42 выполнена
-print("\n После выполнения задач 2, 4 выводим список текущих задач:")
-print_tasks_completed()  # список текущих задач
+store3.add_item('Швабра', 349.99)
+store3.add_item('Тряпка', 49.99)
+store3.add_item('Ведро', 239.99)
 
+print(f"В магазине {store3.name} по адресу: {store3.address} в продаже:\n {store3.items}")
 
+print(f"Цена товара Ведро = {store3.get_price('Ведро')}\n")
+store3.set_price('Ведро', 280)
+print(f"Ведро стало дороже!\nЦена товара Ведро = {store3.get_price('Ведро')}")
 
+store3.remove_item("Тряпка")
+print("Тяпок больше нет!")
+print(f"В магазине {store3.name} по адресу: {store3.address} в продаже:\n {store3.items}")
 
